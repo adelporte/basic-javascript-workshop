7c84e2a2330b03f8d1ab27b36748c5aff114da4a
Augustin Delporte <delporteaugustin@gmail.com>
2016-04-13 17:15:25 +0000

    "Added function firstCharacter"

--- answers.js
+++ answers.js
@@ -1,1 +1,10 @@
-//Hello, this is a comment
+//Returns first character of string
+
+function firstCharacter(word) {
+    return word.substring(0,1);
+}
+
+firstCharacter("Hello");
+firstCharacter("Da");
+firstCharacter("");
+
